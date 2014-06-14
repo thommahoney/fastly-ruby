ef027269855265807e2cb15f5df63ff7c90e73c2
Tyler McMullen <tyler@fastly.com>
2014-06-14 16:00:41 -0700

    Fix remaining test failures so we have something vaguely resembling a clean slate. * success should be 'success' in get_stats * VCL doesn't allow single-quoted strings, thus create_header test was invalid. * Use actual assert_raises for invalid stats tests.

--- lib/fastly/client.rb
+++ lib/fastly/client.rb
@@ -54,7 +54,7 @@ class Fastly
       content = get(path, params)
 
       case content['status']
-      when success then content['data']
+      when 'success' then content['data']
       else
         fail Error, content['message']
       end
